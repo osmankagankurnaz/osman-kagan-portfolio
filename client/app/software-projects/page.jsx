be626154b9ef94d65eb36d03362eae47272180a7
Osman Kagan Kurnaz <osmansaidkurnaz@gmail.com>
2023-07-19 20:32:46 +0300

    created software-projects

--- client/app/software-projects/page.jsx
+++ client/app/software-projects/page.jsx
@@ -1,8 +1,21 @@
+'use client'
 import React from 'react'
+import { Box, Grid, Text } from '@chakra-ui/react'
+import ProjectsCard from '@/components/ProjectCard'
+import softwareProjectsData from '@/mocks/software-projects.json'
 
 const SoftwareProjects = () => {
   return (
-    <div>SoftwareProjects</div>
+    <Box pt={"4rem"} pl={"6rem"} pb={"4rem"}>
+      <Text fontWeight={"semibold"} fontSize={"2rem"} textColor={"white"} mb={"2.5rem"}>Software Projects</Text>
+        <Grid templateColumns='repeat(3, 1fr)' gap={"2rem"} pl={"1.25rem"}>
+        {
+          softwareProjectsData.map((item) => (
+            <ProjectsCard item={item}/>
+          ))
+        }
+        </Grid>
+    </Box>
   )
 }
 
