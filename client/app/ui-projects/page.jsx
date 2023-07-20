bd11a75a0b435822980c587f73d6f9f6b2477741
Osman Kagan Kurnaz <osmansaidkurnaz@gmail.com>
2023-07-20 14:21:23 +0300

    updated ui-projects

--- client/app/ui-projects/page.jsx
+++ client/app/ui-projects/page.jsx
@@ -2,21 +2,18 @@
 import React from 'react'
 import { Box, Grid, Text } from '@chakra-ui/react'
 import ProjectsCard from '@/components/ProjectCard'
+import uiProjectsData from '@/mocks/ui-projects.json'
 
 const UiProjects = () => {
   return (
-    <Box pt={"4rem"} pl={"6rem"}>
+    <Box pt={"4rem"} pl={"6rem"} pb={"8rem"}>
       <Text fontWeight={"semibold"} fontSize={"2rem"} textColor={"white"} mb={"2.5rem"}>UI Projects</Text>
         <Grid templateColumns='repeat(3, 1fr)' gap={"2rem"} pl={"1.25rem"}>
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
-          <ProjectsCard />
+        {
+          uiProjectsData.map((item) => (
+            <ProjectsCard item={item}/>
+          ))
+        }
         </Grid>
     </Box>
   )
