668f379719f077e98396a762dc8809d38ea0b50c
Osman Kagan Kurnaz <osmansaidkurnaz@gmail.com>
2023-07-20 17:36:51 +0300

    fixed all problems

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Providers } from './providers';
 import LeftBar from '@/components/LeftBar';
-import { Box } from '@chakra-ui/react';
+import { Box, useMediaQuery, Text } from '@chakra-ui/react';
 import { Roboto } from "@next/font/google"
 
 const roboto = Roboto({
@@ -11,17 +11,24 @@ const roboto = Roboto({
 })
 
 export default function RootLayout({ children }) {
+  const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)');
   return (
     <html lang="tr">
       <head />
       <body className={roboto.className}>
         <Providers>
-          <Box display={"flex"} >
-            <LeftBar />
-            <Box w={"80vw"} h={"100vh"} bg={"#1F1F1F"} overflow={"auto"}>
-              {children}
+          {isLargerThan1280 ? (
+            <Box display={"flex"} >
+              <LeftBar />
+              <Box w={"80vw"} h={"100vh"} bg={"#1F1F1F"} overflow={"auto"}>
+                {children}
+              </Box>
             </Box>
-          </Box>
+          ) : (
+            <Box w={"100vw"} h={"100vh"} display={"flex"} bg={"#1F1F1F"} textColor={"white"} alignItems={"center"} justifyContent={"center"}>
+              <Text fontSize={"1.5rem"} >Coming soon..</Text>
+            </Box>
+          )}
         </Providers>
       </body>
     </html>
--- client/app/page.jsx
+++ client/app/page.jsx
@@ -7,15 +7,15 @@ import SocialMediaDatas from "@/mocks/social-links.json"
 
 const GettingStarted = () => {
   return (
-    <Box w={"85vw"} h={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"} flexDir={"row"}>
+    <Box h={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"} flexDir={"row"} gap={"4rem"}>
       <Box>
         <Text textColor={"white"} fontWeight={"bold"} fontSize={"1.5rem"}>Hi, I’m Osman</Text>
-        <Text fontSize={"6rem"} fontWeight={"bold"} textColor={"white"} lineHeight={"6rem"} mb={"1rem"}>I’m <span style={{color: "linear-gradient(124deg, #A047FA 0%, #FC41D3 100%)"}}>Developer</span><br />& <span style={{textColor: "linear-gradient(116deg, #FA974F 0%, #F13131 100%)"}}>UI Designer</span></Text>
-        <Text textColor={"white"} fontSize={"1.5rem"} fontWeight={"normal"} mt={"1rem"} w={"32.5rem"} mb={"2rem"}>I am a 3rd year Computer Engineering student at Selcuk University. I do studies and research on the web.</Text>
+        <Text fontSize={"6rem"} fontWeight={"bold"} textColor={"white"} lineHeight={"6rem"} mb={"1rem"}>I’m <span style={{ background: "-webkit-linear-gradient(135deg,#FC41D3 0%, #A047FA 100%)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}>Developer</span><br />& <span style={{ background: "-webkit-linear-gradient(135deg,#F13131 0%, #FA974F 100%)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}>UI Designer</span></Text>
+        <Text textColor={"white"} fontSize={"1.25rem"} fontWeight={"normal"} mt={"1rem"} w={"32.5rem"} mb={"2rem"}>I am a 3rd year Computer Engineering student at Selcuk University. I do studies and research on the web.</Text>
         <Box display={"flex"} flexDir={"row"} gap={"1rem"} mb={"3rem"}>
           {SocialMediaDatas.map((item) => {
             return (
-              <Box key={item.id} boxSize={"4rem"} borderRadius={"full"} overflow={"hidden"}>
+              <Box id={'social-link'} key={item.id} boxSize={"4rem"} borderRadius={"full"} overflow={"hidden"}>
                 <Link href={item.link}>
                   <Image src={item.icon} />
                 </Link>
@@ -25,13 +25,13 @@ const GettingStarted = () => {
           )}
         </Box>
         <Link href={""}>
-          <Button textColor={"white"} bg={"transparent"} border={"1px solid"} borderColor={"white"} _hover={{textColor: "#A147FA", borderColor: "#A147FA"}}>
+          <Button textColor={"white"} bg={"transparent"} border={"1px solid"} borderColor={"white"} _hover={{ textColor: "#1F1F1F", bg: "white" }}>
             Download CV
           </Button>
         </Link>
       </Box>
-      <Box>
-
+      <Box overflow={"hidden"} borderRadius={"10rem"} w={"28rem"} h={"42rem"} border={"2px solid"} borderColor={"white"} >
+          <Image src={"/profile.png"} />
       </Box>
     </Box>
   )
