9e0b52df838441f9e22aecf67206d1cdb473b72c
melihkaan240 <melihkaanarac@gmail.com>
2023-08-28 19:43:34 +0300

    finish projects

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -5,7 +5,7 @@ import LeftBar from '@/components/LeftBar';
 import { Box } from '@chakra-ui/react';
 import { Roboto } from "next/font/google"
 import { Analytics } from '@vercel/analytics/react';
-
+import Topbar from "@/components/Topbar"
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ['400', '500', '700'],
@@ -17,9 +17,10 @@ export default function RootLayout({ children }) {
       <head />
       <body className={roboto.className}>
         <Providers>
-          <Box display={"flex"} >
+          <Box display={"flex"} flexDirection={{base:"column",md:"row"}} >
             <LeftBar />
-            <Box w={"80vw"} h={"100vh"} bg={"#1F1F1F"} overflow={"auto"}>
+            <Topbar/>
+            <Box w={{base:"full",md:"80vw"}} h={"100vh"} bg={"#1F1F1F"} overflowY={"auto"}>
               {children}
             </Box>
           </Box>
--- client/app/skills/page.jsx
+++ client/app/skills/page.jsx
@@ -2,17 +2,13 @@
 import SkillCard from '@/components/SkillCard'
 import { Box, SimpleGrid, Text, CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import React from 'react'
-import { motion } from 'framer-motion'
-
 const Skills = () => {
   return (
-    <Box pt={"4rem"} pl={"6rem"} pb={"2rem"}>
+    <Box pt={"4rem"} pl={{base:0,md:"6rem"}} pb={"2rem"}  display={"flex"} justifyContent={"center"} alignItems={{base:"center",md:"normal"}} flexDirection={"column"}>
       <Text fontWeight={"semibold"} fontSize={"2rem"} textColor={"white"} mb={"2.5rem"}>Skills</Text>
-      <Box pl={"1.25rem"} pr={"7.25rem"} >
-        <SimpleGrid templateColumns={'repeat(4, 1fr)'} spacingY={"8rem"} spacingX={"4rem"}>
-          <motion.div >
-            <SkillCard name={"HTML"} value={90} color={"#EF652B"} />
-          </motion.div>
+      <Box pl={{base:0,md:"1.25rem"}} pr={{base:0,md:"7.25rem"}}  >
+        <SimpleGrid templateColumns={{base:"repeat(2,1fr)",md:'repeat(4, 1fr)'}} pb={{base:"1rem",md:0}} spacingY={{base:"4rem",md:"8rem"}} spacingX={{base:"2rem",md:"4rem"}}>
+          <SkillCard name={"HTML"} value={90} color={"#EF652B"} />
           <SkillCard name={"CSS"} value={70} color={"#2FA7DC"} />
           <SkillCard name={"Javascript"} value={80} color={"#F1BF26"} />
           <SkillCard name={"React.js"} value={60} color={"#4AD5FE"} />
